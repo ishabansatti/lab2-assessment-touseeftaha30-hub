45c9e9277f01af6194ba52974134f4a397e0fcb8
touseeftaha30-hub <touseeftaha30@gmail.com>
2026-02-18 14:50:45 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -16,6 +16,7 @@ float getFloatValue() {
 
 char getCharValue() {
     char var_char = 'A';
+    return var_char;
     // Declare a variable var_char of type character and set its value to A.
     //return var_char;
 }
